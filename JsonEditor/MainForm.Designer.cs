47b31fe2039a163924fc763c3e9bdbe3ee133aa1
KentKaze <kentnewbie@hotmail.com>
2021-01-06 23:03:46 +0900

    Update

--- JsonEditor/MainForm.Designer.cs
+++ JsonEditor/MainForm.Designer.cs
@@ -28,21 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.sspMain = new System.Windows.Forms.StatusStrip();
             this.SuspendLayout();
+            // 
+            // sspMain
+            // 
+            this.sspMain.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.sspMain.Location = new System.Drawing.Point(0, 721);
+            this.sspMain.Name = "sspMain";
+            this.sspMain.Size = new System.Drawing.Size(1275, 22);
+            this.sspMain.TabIndex = 0;
+            this.sspMain.Text = "sspMain";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1275, 743);
+            this.Controls.Add(this.sspMain);
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip sspMain;
     }
 }
 
